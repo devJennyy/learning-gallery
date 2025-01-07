900b8ae6b8cd8ae69026d455192ffc72534be1c7
Jenny Pieloor <devjenny.official@gmail.com>
2025-01-07 22:25:44 +0800

    WIP.

--- src/pages/Tasks/Task01.tsx
+++ src/pages/Tasks/Task01.tsx
@@ -2,11 +2,17 @@ import { IoChevronForwardOutline } from "react-icons/io5";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { FaGithub } from "react-icons/fa";
 import { BiLogoGmail, BiWorld } from "react-icons/bi";
+import { useState } from "react";
 
 const Task01 = () => {
+  const [show, setShow] = useState(false);
+
   return (
-    <div id="task-one" className="flex justify-center sm:items-center w-full h-screen sm:mt-0 mt-14">
-      <div className="flex justify-end items-center border border-l border-primaryStroke h-[30rem] w-80">
+    <div
+      id="task-one"
+      className="flex justify-center w-full sm:mt-32 mt-14"
+    >
+      <div className="flex justify-end items-center border border-l border-primaryStroke h-fit">
         <div className="flex flex-col justify-between items-center h-[400px] w-10 py-2 mr-[2px]">
           <div className="flex justify-center items-center -rotate-90">
             <p className="flex gap-2 uppercase text-[12px] text-secondary/50 font-bold tracking-wider">
@@ -35,7 +41,7 @@ const Task01 = () => {
               className="w-full h-full object-cover"
             />
           </div>
-          <div className="flex flex-col text-start p-5 gap-3">
+          <div className="flex flex-col text-start p-5 gap-3 h-full">
             <h1 className="flex flex-col text-4xl">
               LOREM <span className="text-xl text-[#70DBE3]">IPSUM.</span>
             </h1>
@@ -45,10 +51,17 @@ const Task01 = () => {
               minima aliquid.
             </p>
 
-            <button className="flex justify-center items-center mt-3 w-fit px-3 py-1 border border-primaryStroke bg-secondaryFill rounded-sm gap-1 hover:bg-quaternaryFill duration-300 transition-colors ease-out">
-              <p className="text-[12px]">See more</p>
-              <IoChevronForwardOutline size={12} />
-            </button>
+              <div className={`${show ? "flex flex-col text-start text-sm" : "hidden"}`}>
+                <p className="text-secondary">Email: <span className="text-primary/90">loremipsum@gmail.com</span></p>
+                <p className="text-secondary">Address: <span className="text-primary/90">162 Fairview, Wonderland</span></p>
+              </div>
+              <button
+                onClick={() => setShow((prev) => !prev)}
+                className="flex justify-center items-center mt-3 w-fit px-3 py-[6px] border border-primaryStroke bg-secondaryFill rounded-sm gap-1 hover:bg-quaternaryFill duration-300 transition-colors ease-out"
+              >
+                <p className="text-sm">See {show ? "Less" : "More"}</p>
+                <IoChevronForwardOutline size={12} />
+              </button>
           </div>
         </div>
       </div>
